571ca8d3b596b78b14fd0262a7aa3254da30d57e
unknown <meenawati.tudu@gmail.com>
2020-01-26 23:10:57 +0530

    added subtract feature

--- Calculator.java
+++ Calculator.java
@@ -5,9 +5,14 @@ public class Calculator {
 	public static int add(int a, int b) {
 		return a+b;
 	}
+	
+	public static int substract(int a, int b) {
+		return a-b;
+	}
 
 	public static void main(String[] args) {
 		System.out.println(add(3,5));
+		System.out.println(substract(3,5));
 	}
 
 }
