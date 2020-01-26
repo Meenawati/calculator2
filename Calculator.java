0f767d88d7a2aff4324978f6427a52a7b76cb51a
unknown <meenawati.tudu@gmail.com>
2020-01-26 23:22:54 +0530

    multiply feature done

--- Calculator.java
+++ Calculator.java
@@ -9,10 +9,15 @@ public class Calculator {
 	public static int substract(int a, int b) {
 		return a-b;
 	}
+	
+	public static int multiply(int a, int b) {
+		return a*b;
+	}
 
 	public static void main(String[] args) {
 		System.out.println(add(3,5));
 		System.out.println(substract(3,5));
+		System.out.println(multiply(3,5));
 	}
 
 }
